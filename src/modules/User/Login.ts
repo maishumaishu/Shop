bacdde17a857b42cd79eccdb1a4ceb40ab99caa2
ansiboy <ansiboy@163.com>
2017-02-16 11:59:01 +0800

    重构

--- src/modules/User/Login.ts
+++ src/modules/User/Login.ts
@@ -1,33 +1,38 @@
-﻿// chitu.action(['sv/ShopAdmin', 'site', 'ko.val'], function (page) {
-//     /// <param name="page" type="chitu.Page"/>
-//     var $ctrls = $('#sidebar, #breadcrumbs, #navbar-container > [role="navigation"]');
-//     var model = {
-//         username: ko.observable(''),//admin
-//         password: ko.observable(''),
-//         login: function () {
-//             if (!model.isValid())
-//                 return val.showAllMessages();
+﻿
+import shopAdmin = require('services/ShopAdmin');
+import app = require('Application');
+import site = require('Site');
+import validation = require('knockout.validation');
 
-//             return services.shopAdmin.login(model.username(), model.password())
-//                            .done(function () {
-//                                $('#navbar').find('[name="username"]').text(model.username());
-//                                app.redirect(site.config.startUrl);
-//                                $ctrls.show();
-//                            });
-//         }
-//     };
-
-//     model.username.extend({ required: true });
-//     model.password.extend({ required: true });
-//     var val = ko.validation.group(model);
-
-//     ko.applyBindings(model, page.node());
-//     page.load.add(function () {
-//         $ctrls.hide();
-//     });
-// });
-class LoginPage extends chitu.Page {
-
+export default async function (page: chitu.Page) {
+    requirejs([`text!${page.routeData.actionPath}.html`], (html) => {
+        page.element.innerHTML = html;
+        page_load(page);
+    })
 }
 
-export default LoginPage;
+function page_load(page: chitu.Page) {
+    var $ctrls = $('#sidebar, #breadcrumbs, #navbar-container > [role="navigation"]');
+    var model = {
+        username: ko.observable(''),//admin
+        password: ko.observable(''),
+        login: function () {
+            if (!(model as any).isValid())
+                return val.showAllMessages();
+
+            return shopAdmin.login(model.username(), model.password())
+                .done(function () {
+                    $('#navbar').find('[name="username"]').text(model.username());
+                    app.redirect(site.config.startUrl);
+                    $ctrls.show();
+                });
+        }
+    };
+
+    model.username.extend({ required: true });
+    model.password.extend({ required: true });
+    var val = validation.group(model);
+
+    ko.applyBindings(model, page.element);
+    $ctrls.hide();
+}
