759d2d54e9f44b69a9ce7376519602d62079dc6b
ansiboy <ansiboy@163.com>
2017-02-22 18:35:28 +0800

    完成登出

---
+++ src/modules/User/Logout.ts
@@ -0,0 +1,7 @@
+﻿import app = require('Application');
+import shopAdmin = require('services/ShopAdmin');
+
+export default function (page: chitu.Page) {
+    shopAdmin.logout();
+    app.redirect('User/Login');
+}
