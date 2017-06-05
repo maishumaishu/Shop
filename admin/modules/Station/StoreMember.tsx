36dfb8e2593e9b6761f0891785f4b28197e1690c
ansiboy <ansiboy@163.com>
2017-06-05 15:56:27 +0800

    实现样式切换

--- admin/modules/Station/StoreMember.tsx
+++ admin/modules/Station/StoreMember.tsx
@@ -1,11 +1,12 @@
 import { MobilePageDesigner } from 'modules/Station/Components/MobilePageDesigner';
 import { default as station, PageData, ControlData } from 'services/Station';
 import { default as MemberControl } from 'mobileComponents/member/control';
-
+import { default as StyleControl } from 'mobileComponents/style/control';
 export default function (page: chitu.Page) {
     let pageData = {} as PageData;
     ReactDOM.render(
         <MobilePageDesigner >
             <MemberControl />
+            <StyleControl />
         </MobilePageDesigner>, page.element);
 }
--- admin/modules/Station/StoreStyle.tsx
+++ admin/modules/Station/StoreStyle.tsx
@@ -1,3 +1,14 @@
-export default function () {
-
+import { MobilePageDesigner } from 'modules/Station/Components/MobilePageDesigner';
+import { default as station, PageData, ControlData } from 'services/Station';
+import { default as StyleControl } from 'mobileComponents/style/control';
+import { default as ProductControl } from 'mobileComponents/product/control';
+export default function (page: chitu.Page) {
+    let pageData = {} as PageData;
+    ReactDOM.render(
+        <MobilePageDesigner >
+            <StyleControl style="default" />
+            <ProductControl />
+            <button className="btn btn-primary">TEST</button>
+        </MobilePageDesigner>, page.element);
 }
+
