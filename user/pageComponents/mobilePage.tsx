9860afda9b14aafb28b426e59355d85d96bf582c
ansiboy <ansiboy@163.com>
2017-07-11 11:18:53 +0800

    重构

--- user/pageComponents/mobilePage.tsx
+++ user/pageComponents/mobilePage.tsx
@@ -34,9 +34,12 @@ export interface PageData {
 
 export class MobilePage extends React.Component<Props, { pageData: PageData }>{
     private screenElement: HTMLElement;
+    components: (React.Component<any, any> & { controlId: string })[];
+
     constructor(props) {
         super(props);
         this.state = { pageData: this.props.pageData };
+        this.components = [];
     }
 
     static getInstanceByElement(element: HTMLElement): MobilePage {
@@ -45,17 +48,22 @@ export class MobilePage extends React.Component<Props, { pageData: PageData }>{
 
     static async createControlInstance(controlData: ControlData, element: HTMLElement) {
         let { controlId, controlName, data, selected } = controlData;
-        let controlType = await MobilePage.getControlType(controlName);
-        let reactElement = React.createElement(controlType, data);
+        let types = await MobilePage.getControlType(controlName);
+        let defaultProps = {};
+        if (types.Props) {
+            defaultProps = new types.Props();
+        }
+        data = Object.assign(defaultProps, data || {});
+        let reactElement = React.createElement(types.Control, data);
         let control = ReactDOM.render(reactElement, element);
         control.id = controlId;
-        return { control, controlType };
+        return { control, controlType: types.Control };
     }
 
-    static getControlType(controlName: string): Promise<React.ComponentClass<any>> {
+    static getControlType(controlName: string): Promise<{ Control: React.ComponentClass<any>, Props: { new () } }> {
         return new Promise((resolve, reject) => {
             requirejs([`mobileComponents/${controlName}/control`], function (exports) {
-                resolve(exports.default);
+                resolve({ Control: exports.default, Props: exports.Props });
             })
         })
     }
@@ -68,14 +76,15 @@ export class MobilePage extends React.Component<Props, { pageData: PageData }>{
         return this.renderRuntimeControls(controls);
     }
 
-    static renderControl(controlData: ControlData) {
+    renderControl(controlData: ControlData) {
         let o = controlData;
         let runtimeControl = (
             <div id={o.controlId} key={o.controlId}
-                ref={(e: HTMLElement) => {
+                ref={async (e: HTMLElement) => {
                     if (!e) return;
-                    MobilePage.createControlInstance(o, e);
-
+                    var c = await MobilePage.createControlInstance(o, e);
+                    let obj = Object.assign(c.control, { controlId: o.controlId });
+                    this.components.push(obj);
 
                 }} />
         );
@@ -87,11 +96,11 @@ export class MobilePage extends React.Component<Props, { pageData: PageData }>{
         controls = controls || [];
         return controls.map((o, i) =>
             <div id={o.controlId} key={o.controlId}
-                ref={(e: HTMLElement) => {
+                ref={async (e: HTMLElement) => {
                     if (!e) return;
-                    MobilePage.createControlInstance(o, e);
-
-
+                    var c = await MobilePage.createControlInstance(o, e);
+                    var componet = Object.assign(c.control, { controlId: o.controlId });
+                    this.components.push(componet);
                 }} />
         );
     }
@@ -99,7 +108,7 @@ export class MobilePage extends React.Component<Props, { pageData: PageData }>{
         controls = controls || [];
         return controls.map((o, i) =>
             <div id={o.controlId} key={o.controlId}
-                ref={(e: HTMLElement) => {
+                ref={async (e: HTMLElement) => {
                     if (!e) return;
                     MobilePage.createControlInstance(o, e)
                         .then(data => {
@@ -117,7 +126,9 @@ export class MobilePage extends React.Component<Props, { pageData: PageData }>{
                             }
                         });
 
-
+                    var c = await MobilePage.createControlInstance(o, e);
+                    var componet = Object.assign(c.control, { controlId: o.controlId });
+                    this.components.push(componet);
                 }} />
         );
     }
@@ -180,7 +191,6 @@ export class MobilePage extends React.Component<Props, { pageData: PageData }>{
                 receive: (event: Event, ui: UI) => {
                     let element = ui.helper[0] as HTMLElement;
                     element.removeAttribute('style');
-                    debugger;
                     let controlName = ui.item.attr('data-controlName');
                     console.assert(controlName != null);
                     ui.helper.remove();
@@ -188,7 +198,6 @@ export class MobilePage extends React.Component<Props, { pageData: PageData }>{
                     this.setState(this.state);
                 },
                 update: (event, ui) => {
-                    debugger;
                     let controls = [];
                     for (let i = 0; i < element.children.length; i++) {
                         let child = element.children[i] as HTMLElement;
@@ -212,6 +221,33 @@ export class MobilePage extends React.Component<Props, { pageData: PageData }>{
     render() {
         let children = React.Children.toArray(this.props.children) || [];
         let pageData = this.state.pageData;
+
+        // requirejs(['css!content/devices.css']);
+        // let virtualPhone = (
+            // <div className="marvel-device iphone5c blue">
+            //     <div className="top-bar"></div>
+            //     <div className="sleep"></div>
+            //     <div className="volume"></div>
+            //     <div className="camera"></div>
+            //     <div className="sensor"></div>
+            //     <div className="speaker"></div>
+            //     <div className="screen"
+            //         ref={(e: HTMLElement) => {
+            //             if (!e) return;
+            //             this.screenElement = e;
+            //             (e as any).mobilePage = this;
+            //         }}>
+            //         <PageComponent>
+            //             {this.renderHeader(pageData)}
+            //             {this.renderViews(pageData)}
+            //             {this.renderFooter(pageData)}
+            //         </PageComponent>
+            //     </div>
+            //     <div className="home"></div>
+            //     <div className="bottom-bar"></div>
+            // </div>
+        // );
+        // return virtualPhone;
         return (
             <div>
                 <PageComponent>
