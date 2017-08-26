deb6ee6e9c1ffe9168a30e7748954ba559508ada
maishu <ansiboy@163.com>
2017-08-26 23:40:17 +0800

    取消 fetch 自定义

--- lib/typings/fetch.d.ts
+++ lib/typings/fetch.d.ts
@@ -1,24 +1,24 @@
-interface FetchOptions {
-    method?: string,
-    headers?: any,
-    body?: any,
-}
+// interface FetchOptions {
+//     method?: string,
+//     headers?: any,
+//     body?: any,
+// }
 
-interface FetchResponse {
-    json(): any;
-    text(): string | Promise<string>;
-    status: number;
-    statusText: string;
-    headers: FetchHeader;
-}
+// interface FetchResponse {
+//     json(): any;
+//     text(): string | Promise<string>;
+//     status: number;
+//     statusText: string;
+//     headers: FetchHeader;
+// }
 
-interface FetchHeader {
-    get(name: string): string
-}
+// interface FetchHeader {
+//     get(name: string): string
+// }
 
-declare function fetch(url: string, options: FetchOptions): Promise<FetchResponse>
+// declare function fetch(url: string, options: FetchOptions): Promise<FetchResponse>
 
 
-declare module "fetch" {
-    export = fetch;
-}
+// declare module "fetch" {
+//     export = fetch;
+// }
