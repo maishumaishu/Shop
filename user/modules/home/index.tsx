08b9f0f1f4348c324c144157a5a27208b26f28d0
“myname” <myname@server>
2017-10-25 09:41:34 +0800

    fix 首页展示

--- user/modules/home/index.tsx
+++ user/modules/home/index.tsx
@@ -4,10 +4,10 @@ import { StationService } from 'userServices/stationService';
 import { MobilePage } from 'mobileComponents/mobilePage';
 import * as ReactDOM from 'react-dom';
 
- 
+
 export default async function (page: Page) {
     let station = page.createService(StationService);
-    let pageData = await station.defaultPageData();
+    let pageData = await station.fullPage(station.homePage);
     ReactDOM.render(
         <MobilePage pageData={pageData} />
         , page.element);
