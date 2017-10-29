92ef48aa0858a2b46aa3d703d60f96e5bef4434f
“myname” <myname@server>
2017-10-29 22:38:42 +0800

    登录页面不用 controls

--- user/modules/user/login.tsx
+++ user/modules/user/login.tsx
@@ -3,7 +3,7 @@ import * as ui from 'ui';
 import { MemberService } from 'userServices/memberService';
 import { app } from 'site';
 import FormValidator from 'formValidator';
-let { PageComponent, PageHeader, PageFooter, PageView, DataList } = controls;
+// let { PageComponent, PageHeader, PageFooter, PageView, DataList } = controls;
 
 export default function (page: Page) {
     let member = page.createService(MemberService);
@@ -12,61 +12,65 @@ export default function (page: Page) {
     let formElement: HTMLFormElement;
     let validator: FormValidator;
 
+    class UserLoginPage extends React.Component<any, any> {
+        render() {
+            return (
+                <div>
+                    <header>
+                        {defaultNavBar({ title: "登录" })}
+                    </header>
+                    <section>
+                        <form className="form-horizontal container"
+                            ref={(e: HTMLFormElement) => formElement = e || formElement}>
+                            <div className="form-group">
+                                <div className="col-xs-12">
+                                    <input type="text" name="username" className="form-control" placeholder="手机号码"
+                                        ref={(e: HTMLInputElement) => usernameInput = e || usernameInput} />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="col-xs-12">
+                                    <input type="password" name="password" className="form-control" placeholder="密码"
+                                        ref={(e: HTMLInputElement) => passwordInput = e || passwordInput} />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="col-xs-12">
+                                    <button id="Login" type="button" className="btn btn-primary btn-block"
+                                        ref={(e: HTMLButtonElement) => {
+                                            if (!e) return;
+                                            e.onclick = ui.buttonOnClick(async () => {
+                                                if (!validator) {
+                                                    validator = new FormValidator(formElement, {
+                                                        username: {
+                                                            rules: ['required', 'mobile'],
+                                                            display: '手机号码'
+                                                        },
+                                                        password: { rules: ['required'], display: '密码' }
+                                                    });
+                                                }
+
+                                                if (!validator.validateForm())
+                                                    return;
+
+                                                await member.login(usernameInput.value, passwordInput.value);
+                                                app.redirect(returnString);
+                                            });
+                                        }}>立即登录</button>
+                                </div>
+                                <div className="col-xs-12 text-center" style={{ marginTop: 10 }}>
+                                    <a href="#user_register" className="pull-left">我要注册</a>
+                                    <a href="#user_resetPassword" className="pull-right">忘记密码</a>
+                                </div>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+            );
+        }
+    }
+
     let returnString = page.routeData.values.reutrn || 'user_index';
 
-    var jsx =
-        <PageComponent>
-            <PageHeader>
-                {defaultNavBar({ title: "登录" })}
-            </PageHeader>
-            <PageFooter></PageFooter>
-            <PageView>
-                <form className="form-horizontal container"
-                    ref={(e: HTMLFormElement) => formElement = e || formElement}>
-                    <div className="form-group">
-                        <div className="col-xs-12">
-                            <input type="text" name="username" className="form-control" placeholder="手机号码"
-                                ref={(e: HTMLInputElement) => usernameInput = e || usernameInput} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-xs-12">
-                            <input type="password" name="password" className="form-control" placeholder="密码"
-                                ref={(e: HTMLInputElement) => passwordInput = e || passwordInput} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-xs-12">
-                            <button id="Login" type="button" className="btn btn-primary btn-block"
-                                ref={(e: HTMLButtonElement) => {
-                                    if (!e) return;
-                                    e.onclick = ui.buttonOnClick(async () => {
-                                        if (!validator) {
-                                            validator = new FormValidator(formElement, {
-                                                username: {
-                                                    rules: ['required', 'mobile'],
-                                                    display: '手机号码'
-                                                },
-                                                password: { rules: ['required'], display: '密码' }
-                                            });
-                                        }
-
-                                        if (!validator.validateForm())
-                                            return;
-
-                                        await member.login(usernameInput.value, passwordInput.value);
-                                        app.redirect(returnString);
-                                    });
-                                }}>立即登录</button>
-                        </div>
-                        <div className="col-xs-12 text-center" style={{ marginTop: 10 }}>
-                            <a href="#user_register" className="pull-left">我要注册</a>
-                            <a href="#user_resetPassword" className="pull-right">忘记密码</a>
-                        </div>
-                    </div>
-                </form>
-            </PageView>
-        </PageComponent>;
-
-    ReactDOM.render(jsx, page.element);
+    ReactDOM.render(<UserLoginPage />, page.element);
 }
