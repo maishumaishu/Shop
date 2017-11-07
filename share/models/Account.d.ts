7bcd049e05b4050da88e580b612814be59a4477a
“myname” <myname@server>
2017-11-07 18:34:10 +0800

    更新 model

--- share/models/Account.d.ts
+++ share/models/Account.d.ts
@@ -2,3 +2,17 @@ interface Account {
     UserId: string;
     Balance: number;
 }
+
+interface BalanceDetail {
+    Amount: number
+    Balance: number
+    CreateDateTime: Date
+    Type: 'OrderPurchase' | 'OrderCancel' | 'OnlineRecharge' | 'StoreRecharge'
+}
+
+interface ScoreDetail {
+    Score: number
+    Balance: number
+    CreateDateTime: Date
+    Type: 'OrderPurchase' | 'OrderConsume'
+}
