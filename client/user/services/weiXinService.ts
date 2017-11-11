80c3f023dd78b6f59e11d9086293ce07863c5b0e
“myname” <myname@server>
2017-11-11 23:14:57 +0800

    添加 weixin js 函数

--- client/user/services/weiXinService.ts
+++ client/user/services/weiXinService.ts
@@ -1,4 +1,36 @@
 import { Service, parseUrlParams } from 'userServices/service';
+
+var ua = navigator.userAgent.toLowerCase();
+export let isWeixin = (ua.match(/MicroMessenger/i) as any) == 'micromessenger';
+
+export function createWeixinClient(weixin: WeiXinService) {
+    return new Promise<jweixin>((resolve, reject) => {
+        var config = {
+            debug: false,
+            nonceStr: 'mystore',
+            jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'getLocation', 'chooseImage', 'getLocalImgData']
+        };
+
+        requirejs(['jweixin'], function (wx: jweixin) {
+            var url = encodeURIComponent(location.href.split('#')[0]);
+            weixin.jsSignature(config.nonceStr, url).then(function (obj) {
+                config = Object.assign(config, obj);
+                wx.config(config);
+                wx.ready(function () {
+                    resolve(wx);
+                });
+                wx.error((res) => {
+                    let error = new Error();
+                    error.message = res.errMsg;
+                    reject(error);
+                });
+            });
+        }, function (err) {
+            reject(err);
+        })
+    })
+}
+
 export class WeiXinService extends Service {
     constructor() {
         super();
@@ -8,6 +40,11 @@ export class WeiXinService extends Service {
     }
     async openid() {
         return localStorage.getItem('openid');
+    }
+    jsSignature = (noncestr, url) => {
+        var data = { noncestr: noncestr, url: url };
+        let u = this.url('WeiXin/GetJsSignature');
+        return this.get(u, data);
     }
 }
 
