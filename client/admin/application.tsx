1802ffc5d70b4c5ca9f84c61c9d989b31cb39ec0
maishu <ansiboy@163.com>
2018-03-24 10:42:08 +0800

    refelect application

--- client/admin/application.tsx
+++ client/admin/application.tsx
@@ -1,192 +1,22 @@
 ﻿import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { menuData, MenuNode } from 'menuData';
 import * as ui from 'ui';
+import { menuData, MenuNode } from 'menuData';
 import { Service } from 'services/service';
 import { shopName } from 'share/common';
+import { MasterPage } from 'masterPage';
 
-ui.dialogConfig.dialogContainer = document.querySelector('.dialog-container') as HTMLElement;
-
-let viewContainer: HTMLElement;
-
-var h = React.createElement;
-class Page extends React.Component<any, {
-    currentNode: MenuNode, username: string
-}> {
-    constructor(props) {
-        super(props);
-        Service.adminName.add((value) => {
-            this.state.username = value;
-            this.setState(this.state);
-        })
-
-        let url = (window.location.hash.substr(1) || '').split('?')[0];
-        this.state = { currentNode: this.findNodeByUrl(url), username: Service.adminName.value };
-
-
-    }
-
-    updateMenu(page: chitu.Page) {
-        let url = page.name.replace(/\./, '/'); //(window.location.hash.substr(1) || '').split('?')[0];
-        let currentNode = this.findNodeByUrl(url);
-        // if (currentNode != null && currentNode == this.state.currentNode)
-        //     return;
-
-        this.state.currentNode = currentNode;
-        this.state.username = Service.adminName.value;
-        this.setState(this.state);
-    }
-
-    findNodeByUrl(url: string): MenuNode {
-        let stack = new Array<MenuNode>();
-        for (let i = 0; i < menuData.length; i++) {
-            stack.push(menuData[i]);
-        }
-        while (stack.length > 0) {
-            let node = stack.pop();
-            if (node.Url == url) {
-                return node;
-            }
-            let children = node.Children || [];
-            for (let j = 0; j < children.length; j++) {
-                stack.push(children[j]);
-            }
-        }
-        return null;
-    }
-    showPageByNode(node: MenuNode) {
-        let url = node.Url;
-        if (url == null && node.Children.length > 0) {
-            node = node.Children[0];
-            url = node.Url;
-        }
-
-        if (url == null && node.Children.length > 0) {
-            node = node.Children[0];
-            url = node.Url;
-        }
-
-        if (url) {
-            app.redirect(url);
-        }
-
-        this.state.currentNode = node;
-        this.setState(this.state);
-    }
-    render() {
-        let currentNode = this.state.currentNode;
-        let firstLevelNode: MenuNode;
-        let secondLevelNode: MenuNode;
-        let thirdLevelNode: MenuNode;
-
-        // if (currentNode == null) {
-        //     return (
-        //         <div ref={(e: HTMLElement) => viewContainer = e || viewContainer}>
-
-        //         </div>
-        //     );
-        // }
-
-        let firstLevelNodes = menuData.filter(o => o.Visible == null || o.Visible == true);
-        let secondLevelNodes: Array<MenuNode> = [];
-        let thirdLevelNodes: Array<MenuNode> = [];
-        if (currentNode != null) {
-            if (currentNode.Parent == null) {
-                firstLevelNode = currentNode;
-                secondLevelNode = currentNode;
-            }
-            else if (currentNode.Parent.Parent == null) {
-                firstLevelNode = currentNode.Parent;
-                secondLevelNode = currentNode;
-            }
-            else if (currentNode.Parent.Parent.Parent == null) {
-                thirdLevelNode = currentNode;
-                secondLevelNode = thirdLevelNode.Parent;
-                firstLevelNode = secondLevelNode.Parent;
-            }
-            else if (currentNode.Parent.Parent.Parent.Parent == null) {
-                thirdLevelNode = currentNode.Parent;
-                secondLevelNode = thirdLevelNode.Parent;
-                firstLevelNode = secondLevelNode.Parent;
-            }
-            else {
-                throw new Error('not implement')
-            }
-        }
-
-        if (firstLevelNode != null) {
-            secondLevelNodes = firstLevelNode.Children || [].filter(o => o.Visible == null || o.Visible == true);
-            thirdLevelNodes = (secondLevelNode.Children || []).filter(o => o.Visible == null || o.Visible == true);
-        }
-
-        if (thirdLevelNodes.length == 0) {
-            thirdLevelNodes.push(secondLevelNode);
-            thirdLevelNode = secondLevelNode;
-        }
-
-        let nodeClassName = '';
-        if (firstLevelNode != null && firstLevelNode.Title == 'Others') {
-            nodeClassName = 'hideFirst';
-        }
-        else if (secondLevelNodes.length == 0) {
-            nodeClassName = 'hideSecond';
-        }
-
-        // let currentPageName = app.currentPage != null ? app.currentPage.name : '';
-        let hideExistsButton = location.hash == '#user/login' || !Service.token;
-
-        return (
-            <div className={nodeClassName}>
-                <div className="first admin-pc">
-                    <ul className="list-group" style={{ margin: 0 }}>
-                        {firstLevelNodes.map((o, i) =>
-                            <li key={i} className={o == firstLevelNode ? "list-group-item active" : "list-group-item"}
-                                style={{ cursor: 'pointer', display: o.Visible == false ? "none" : null }}
-                                onClick={() => this.showPageByNode(o)}>
-                                <i className={o.Icon} style={{ fontSize: 16 }}></i>
-                                <span style={{ paddingLeft: 8, fontSize: 14 }}>{o.Title}</span>
-                            </li>
-                        )}
-                    </ul>
-                </div>
-                <div className="second admin-pc">
-                    <ul className="list-group" style={{ margin: 0 }}>
-                        {secondLevelNodes.map((o, i) =>
-                            <li key={i} className={o == secondLevelNode ? "list-group-item active" : "list-group-item"}
-                                style={{ cursor: 'pointer', display: o.Visible == false ? "none" : null }}
-                                onClick={() => this.showPageByNode(o)}>
-                                <span style={{ paddingLeft: 8, fontSize: 14 }}>{o.Title}</span>
-                            </li>
-                        )}
-                    </ul>
-                </div>
-                <div className={secondLevelNodes.length == 0 ? "main hideSecond" : 'main'} >
-                    <nav className="navbar navbar-default">
-                        <div className="container-fluid">
-                            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
-                                <ul className="nav navbar-nav" style={{ width: '100%' }}>
-                                    {!hideExistsButton ?
-                                        <li className="light-blue pull-right" style={{ color: 'white', paddingTop: 12, cursor: 'pointer' }}
-                                            onClick={() => location.hash = '#user/login'}>
-                                            <i className="icon-off"></i>
-                                            <span style={{ paddingLeft: 4 }}>退出</span>
-                                        </li> : null
-                                    }
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-                    <div style={{ padding: 20 }}
-                        ref={(e: HTMLElement) => viewContainer = e || viewContainer}>
-                    </div>
-                </div>
-            </div >
-        );
-    }
+function main(): Application {
+    ui.dialogConfig.dialogContainer = document.querySelector('.dialog-container') as HTMLElement;
+    let app: Application = window['admin-app'] = window['admin-app'] || new Application();
+    return app;
 }
+
+export default main();
 
 class Application extends chitu.Application {
 
+    private masterPage: MasterPage;
     constructor() {
         super();
 
@@ -194,7 +24,20 @@ class Application extends chitu.Application {
             this.redirect('user/login');
         }
 
-        this.pageCreated.add((app, page) => {
+        this.createMasterPage();
+        this.error.add((app, err) => this.errorHandle(err));
+    }
+
+    createMasterPage() {
+        let element = document.createElement('div');
+        document.body.insertBefore(element, document.body.children[0]);
+        this.masterPage = ReactDOM.render(<MasterPage app={this} />, element);
+        document.title = shopName;
+
+        this.pageCreated.add((sender, page) => {
+            page.showing.add((sender) => this.masterPage.updateMenu(sender));
+            page.hiding.add((sender) => this.masterPage.updateMenu(sender));
+
             page.load.add((sender, args) => {
                 let element = sender.element.querySelector('admin-pc');
                 if (element == null) {
@@ -202,16 +45,14 @@ class Application extends chitu.Application {
                 }
             });
         })
-
-        this.error.add((app, err) => this.errorHandle(err));
     }
 
     protected createPageElement(routeData: chitu.RouteData): HTMLElement {
         let element = document.createElement('div');
-        console.assert(viewContainer != null, 'view container cannt be null.');
+        console.assert(this.masterPage.viewContainer != null, 'view container cannt be null.');
         let className = routeData.pageName.split('.').join('-');
         element.className = className;
-        viewContainer.appendChild(element);
+        this.masterPage.viewContainer.appendChild(element);
 
         return element;
     }
@@ -227,12 +68,12 @@ class Application extends chitu.Application {
             case '710':
             case '724':     //724 为 token 失效
             case '601':     //601 为用户未登录异常
-                var currentPage = app.currentPage;
+                var currentPage = this.currentPage;
                 let isLoginPage = currentPage.name == 'user.login';
                 if (isLoginPage) {
                     return;
                 }
-                app.redirect('user_login', { return: currentPage.routeData.routeString });
+                this.redirect('user_login', { return: currentPage.routeData.routeString });
                 break;
             case '725':
                 ui.alert({ title: '错误', message: 'application-key 配置错误' });
@@ -240,20 +81,6 @@ class Application extends chitu.Application {
         }
     }
 }
-
-
-let element = document.createElement('div');
-document.body.insertBefore(element, document.body.children[0]);
-let p: Page = ReactDOM.render(<Page />, element);
-document.title = shopName;
-let app: Application = window['admin-app'] = window['admin-app'] || new Application();
-
-app.pageCreated.add((sender, page) => {
-    page.showing.add((sender) => p.updateMenu(sender));
-    page.hiding.add((sender) => p.updateMenu(sender));
-})
-
-export default app;
 
 
 
---
+++ client/admin/masterPage.tsx
@@ -0,0 +1,183 @@
+import { menuData, MenuNode } from 'menuData';
+import { Service } from 'services/service';
+
+interface State {
+    currentNode: MenuNode,
+    username: string
+}
+
+interface Props {
+    app: chitu.Application
+}
+
+export class MasterPage extends React.Component<Props, State> {
+    viewContainer: any;
+    constructor(props) {
+        super(props);
+        Service.adminName.add((value) => {
+            this.state.username = value;
+            this.setState(this.state);
+        })
+
+        let url = (window.location.hash.substr(1) || '').split('?')[0];
+        this.state = { currentNode: this.findNodeByUrl(url), username: Service.adminName.value };
+
+
+    }
+
+    updateMenu(page: chitu.Page) {
+        let url = page.name.replace(/\./, '/'); 
+        let currentNode = this.findNodeByUrl(url);
+
+        this.state.currentNode = currentNode;
+        this.state.username = Service.adminName.value;
+        this.setState(this.state);
+    }
+
+    findNodeByUrl(url: string): MenuNode {
+        let stack = new Array<MenuNode>();
+        for (let i = 0; i < menuData.length; i++) {
+            stack.push(menuData[i]);
+        }
+        while (stack.length > 0) {
+            let node = stack.pop();
+            if (node.Url == url) {
+                return node;
+            }
+            let children = node.Children || [];
+            for (let j = 0; j < children.length; j++) {
+                stack.push(children[j]);
+            }
+        }
+        return null;
+    }
+    showPageByNode(node: MenuNode) {
+        let url = node.Url;
+        if (url == null && node.Children.length > 0) {
+            node = node.Children[0];
+            url = node.Url;
+        }
+
+        if (url == null && node.Children.length > 0) {
+            node = node.Children[0];
+            url = node.Url;
+        }
+
+        if (url) {
+            this.props.app.redirect(url);
+        }
+
+        this.state.currentNode = node;
+        this.setState(this.state);
+    }
+    render() {
+        let currentNode = this.state.currentNode;
+        let firstLevelNode: MenuNode;
+        let secondLevelNode: MenuNode;
+        let thirdLevelNode: MenuNode;
+
+        // if (currentNode == null) {
+        //     return (
+        //         <div ref={(e: HTMLElement) => viewContainer = e || viewContainer}>
+
+        //         </div>
+        //     );
+        // }
+
+        let firstLevelNodes = menuData.filter(o => o.Visible == null || o.Visible == true);
+        let secondLevelNodes: Array<MenuNode> = [];
+        let thirdLevelNodes: Array<MenuNode> = [];
+        if (currentNode != null) {
+            if (currentNode.Parent == null) {
+                firstLevelNode = currentNode;
+                secondLevelNode = currentNode;
+            }
+            else if (currentNode.Parent.Parent == null) {
+                firstLevelNode = currentNode.Parent;
+                secondLevelNode = currentNode;
+            }
+            else if (currentNode.Parent.Parent.Parent == null) {
+                thirdLevelNode = currentNode;
+                secondLevelNode = thirdLevelNode.Parent;
+                firstLevelNode = secondLevelNode.Parent;
+            }
+            else if (currentNode.Parent.Parent.Parent.Parent == null) {
+                thirdLevelNode = currentNode.Parent;
+                secondLevelNode = thirdLevelNode.Parent;
+                firstLevelNode = secondLevelNode.Parent;
+            }
+            else {
+                throw new Error('not implement')
+            }
+        }
+
+        if (firstLevelNode != null) {
+            secondLevelNodes = firstLevelNode.Children || [].filter(o => o.Visible == null || o.Visible == true);
+            thirdLevelNodes = (secondLevelNode.Children || []).filter(o => o.Visible == null || o.Visible == true);
+        }
+
+        if (thirdLevelNodes.length == 0) {
+            thirdLevelNodes.push(secondLevelNode);
+            thirdLevelNode = secondLevelNode;
+        }
+
+        let nodeClassName = '';
+        if (firstLevelNode != null && firstLevelNode.Title == 'Others') {
+            nodeClassName = 'hideFirst';
+        }
+        else if (secondLevelNodes.length == 0) {
+            nodeClassName = 'hideSecond';
+        }
+
+        // let currentPageName = app.currentPage != null ? app.currentPage.name : '';
+        let hideExistsButton = location.hash == '#user/login' || !Service.token;
+
+        return (
+            <div className={nodeClassName}>
+                <div className="first admin-pc">
+                    <ul className="list-group" style={{ margin: 0 }}>
+                        {firstLevelNodes.map((o, i) =>
+                            <li key={i} className={o == firstLevelNode ? "list-group-item active" : "list-group-item"}
+                                style={{ cursor: 'pointer', display: o.Visible == false ? "none" : null }}
+                                onClick={() => this.showPageByNode(o)}>
+                                <i className={o.Icon} style={{ fontSize: 16 }}></i>
+                                <span style={{ paddingLeft: 8, fontSize: 14 }}>{o.Title}</span>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                <div className="second admin-pc">
+                    <ul className="list-group" style={{ margin: 0 }}>
+                        {secondLevelNodes.map((o, i) =>
+                            <li key={i} className={o == secondLevelNode ? "list-group-item active" : "list-group-item"}
+                                style={{ cursor: 'pointer', display: o.Visible == false ? "none" : null }}
+                                onClick={() => this.showPageByNode(o)}>
+                                <span style={{ paddingLeft: 8, fontSize: 14 }}>{o.Title}</span>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                <div className={secondLevelNodes.length == 0 ? "main hideSecond" : 'main'} >
+                    <nav className="navbar navbar-default">
+                        <div className="container-fluid">
+                            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
+                                <ul className="nav navbar-nav" style={{ width: '100%' }}>
+                                    {!hideExistsButton ?
+                                        <li className="light-blue pull-right" style={{ color: 'white', paddingTop: 12, cursor: 'pointer' }}
+                                            onClick={() => location.hash = '#user/login'}>
+                                            <i className="icon-off"></i>
+                                            <span style={{ paddingLeft: 4 }}>退出</span>
+                                        </li> : null
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                    <div style={{ padding: 20 }}
+                        ref={(e: HTMLElement) => this.viewContainer = e || this.viewContainer}>
+                    </div>
+                </div>
+            </div >
+        );
+    }
+}
