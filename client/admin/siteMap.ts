0d971216822f83f632456afa410ab7e08abb83c8
maishu-PC\maishu <ansiboy@163.com>
2018-04-15 00:21:19 +0800

    添加缺失的页面

--- client/admin/siteMap.ts
+++ client/admin/siteMap.ts
@@ -1,29 +1,57 @@
+let dir = 'modules';
 let siteMap = {
     nodes: {
-        "user/login": { action: 'modules/user/login' },
+        "home/index": {
+            action: `${dir}/home/index`
+        },
+        "coupon/couponList": {
+            action: `${dir}/coupon/couponList`
+        },
+        "coupon/couponCodeList": {
+            action: `${dir}/coupon/couponCodeList`
+        },
+        "user/login": {
+            action: 'modules/user/login'
+        },
+        "shopping/productList": {
+            action: `${dir}/shopping/productList`
+        },
+        "shopping/orderList": {
+            action: `${dir}/shopping/orderList`,
+        },
+        "shopping/promotion/activities": {
+            action: `${dir}/shopping/promotion/activities`
+        },
         "station/index": {
             title: '店铺设置',
-            action: 'modules/station/index'
+            action: `${dir}/station/index`
         },
         "station/pages/home": {
             title: '店铺首页',
-            action: 'modules/station/pages/home'
+            action: `${dir}/station/pages/home`
         },
         "station/shoppingCart": {
-            action: 'modules/station/shoppingCart'
+            action: `${dir}/station/shoppingCart`
         },
         "station/storeMember": {
             title: '会员主页',
-            action: 'modules/station/storeMember'
+            action: `${dir}/station/storeMember`
         },
         "station/storeCategories": {
-            action: 'modules/station/storeCategories',
+            action: `${dir}/station/storeCategories`,
         },
         "station/storeMenu": {
             title: '店铺导航',
-            action: 'modules/station/storeMenu'
+            action: `${dir}/station/storeMenu`
         },
-        "user/myStores": { action: 'modules/user/myStores' },
+        "station/storeStyle": {
+            title: '店铺风格',
+            action: `${dir}/station/storeStyle`
+        },
+        "user/myStores": {
+            title: '我的店铺',
+            action: `${dir}/user/myStores`
+        },
         "shopping/promotion/activityEdit": { action: "modules/shopping/promotion/activityEdit" },
         "shopping/product/productEdit": { action: "modules/shopping/product/productEdit" },
         productEdit: { action: "modules/shopping/product/productEdit" },
