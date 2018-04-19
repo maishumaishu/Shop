ad13d562e11339eeafb887a49b7281597bb7229f
maishu-PC\maishu <ansiboy@163.com>
2018-04-19 13:48:39 +0800

    重构店铺页面

--- client/admin/modules/station/storePageDesign.tsx
+++ client/admin/modules/station/storePageDesign.tsx
@@ -4,11 +4,20 @@ import { StationService as UserStation } from 'userServices/stationService';
 import { AppError } from 'share/common'
 export default async function (page: chitu.Page) {
 
+    let arr = page.name.split('_');
+    let storePage = arr[arr.length - 1];
+
     let adminStation = page.createService(AdminStation);
     let userStation = page.createService(UserStation);
-    let pageData = await userStation.pages.shoppingCart();
+
+    let func = userStation.pages[storePage] as Function;
+    if (func == null)
+        throw new Error(`Store page ${storePage} is not exists.`);
+
+    let pageData = await func.apply(userStation.pages);
+
     ReactDOM.render(
         <MobilePageDesigner pageData={pageData} showMenuSwitch={true}
-            save={adminStation.savePageData.bind(adminStation)} userStation={userStation}>
+            save={adminStation.savePageData.bind(adminStation)} pageDatas={userStation.pages}>
         </MobilePageDesigner>, page.element);
 }
