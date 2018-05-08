5b3e83246e1aca59d525bf31573f4d7d56f31ceb
maishu-PC\maishu <ansiboy@163.com>
2018-05-08 13:15:27 +0800

    移除 js

--- client/admin/modules/finance/income.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    function default_1(page) {
-    }
-    exports.default = default_1;
-});
