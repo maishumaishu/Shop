46bd66eff4a63c0a120d1a85e8b821a976492bec
maishu-PC\maishu <ansiboy@163.com>
2018-05-09 08:51:09 +0800

    remove admin js css

--- client/admin/siteMap.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", "admin/pageNodes"], function (require, exports, pageNodes_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.siteMap = pageNodes_1.siteMap;
-});
